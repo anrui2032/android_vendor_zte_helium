c2a41da666c379df22af19a1eb988ea8f40aa7d3
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    helium: Declare IMS libs as symlinks during extraction
    Change-Id: Ia1b8097d28dd4888232fe383b830515b1b9bafaa

--- helium-vendor.mk
+++ helium-vendor.mk
@@ -1343,3 +1343,7 @@ PRODUCT_PACKAGES += \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
     vendor.qti.gnss@4.0-service.xml \
     vendor.qti.hardware.display.composer-service.xml
+
+PRODUCT_PACKAGES += \
+    system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
+    system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so
