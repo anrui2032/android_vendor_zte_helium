f867f121c53f9c612ceae8d84af34106e1166f86
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:03:24 +0300

    helium: fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09

--- helium-vendor.mk
+++ helium-vendor.mk
@@ -924,7 +924,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsconfig@2.0 \
     vendor.qti.ims.rcsconfig@2.1 \
     vendor.qti.imsrtpservice@3.0-service-Impl \
-    vendor.qti.imsrtpservice@3.0 \
+    vendor.qti.imsrtpservice@3.0_vendor \
     vendor.qti.latency@2.0 \
     vendor.zte.fingerprint.ifaahidl@1.0 \
     vendor.zte.hardware.radio@1.0 \
