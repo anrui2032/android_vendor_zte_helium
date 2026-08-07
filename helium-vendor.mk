428ee81b4046f5f485c9cd5af1279706d628f559
anrui2032 <anrui2032@gmail.com>
2026-08-08 03:35:52 +0800

    helium: Import touchscreen firmware
     From: GEN_ZTE_P725A02V2.0.0B21
    
    Change-Id: I375ef6e2295534b99c19bc2ef0c6c3ae8b6eec98

--- helium-vendor.mk
+++ helium-vendor.mk
@@ -423,6 +423,8 @@ PRODUCT_COPY_FILES += \
     vendor/zte/helium/proprietary/vendor/firmware/a650_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a650_zap.b02 \
     vendor/zte/helium/proprietary/vendor/firmware/a650_zap.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/a650_zap.elf \
     vendor/zte/helium/proprietary/vendor/firmware/a650_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a650_zap.mdt \
+    vendor/zte/helium/proprietary/vendor/firmware/goodix_cfg_group.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_cfg_group.bin \
+    vendor/zte/helium/proprietary/vendor/firmware/goodix_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware.bin \
     vendor/zte/helium/proprietary/vendor/firmware/ipa_fws.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b00 \
     vendor/zte/helium/proprietary/vendor/firmware/ipa_fws.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b01 \
     vendor/zte/helium/proprietary/vendor/firmware/ipa_fws.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b02 \
